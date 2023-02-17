433cb291a24a52aaab8440eaf506555dc50298df
Debbie <debby.ek@yahoo.com>
2023-02-17 16:58:56 -0600

    added event listeners for read buttons

--- java.js
+++ java.js
@@ -24,10 +24,7 @@ let pages = document.querySelector("#pages");
     pages.addEventListener('input', ()=> {
         this.pages = pages.value;
     });
-let hasRead = document.querySelector("#hasRead");
-    hasRead.addEventListener('input', ()=> {
-        this.hasRead = hasRead.value;
-    });
+
 
 let janeEyre = document.querySelector('#janeEyre');
 let dracula = document.querySelector('#dracula');
@@ -37,28 +34,31 @@ let myLibrary = [];
 myLibrary.push('janeEyre');
 myLibrary.push('dracula');
 myLibrary.push('jekyllHyde');
-console.log(myLibrary);
+    
 
-function book (title, author, pages, hasRead) {
+function book (title, author, pages, ) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
+    this.pages = pages + " pages";
+   
 
     this.info =function() {
-        return this.title + this.author + this.pages + this.read
+        return this.title + this.author + this.pages
     }
     console.log(this.info);
-    myLibrary.push('this.info');
+   
 };
 
 let library = document.querySelector(".library");
-
+let books = document.querySelectorAll(".bookCard");
+let newBook;
 function addBookToLibrary() {
+    
     submit.addEventListener( 'click', () => {
-        book(title, author, pages, hasRead);
+        newBook = new book(title, author, pages);
+        myLibrary.push('newBook');
         popUp.style.display = "none";
-
+        event.preventDefault();
         let card = document.createElement('div');
         card.classList.add('bookCard');
         card.style.height = "200px";
@@ -66,38 +66,102 @@ function addBookToLibrary() {
         card.style.backgroundColor = "#e5e7e";
         card.style.boxShadow = "0px 7px 17px blue";
         card.style.borderRadius = "8px";
-        library.appendChild(card);
+        card.setAttribute("id", `${title}`);
+        
+       
         
         let firstLine = document.createElement( 'h3');
-            firstLine.textContent = `${this.title}`;
+            firstLine.innerHTML = `${this.title}`;
             firstLine.style.fontFamily = "monospace";
             firstLine.style.borderBottom = "dotted 1px black";
             firstLine.style.paddingTop = "20px";
             card.appendChild(firstLine);
         let secondLine = document.createElement( 'h3');  
-            secondLine.textContent = `${this.author}`;
+            secondLine.innerHTML = `${this.author}`;
             secondLine.style.fontFamily = "monospace";
             secondLine.style.borderBottom = "dotted 1px black";
             secondLine.style.paddingTop = "20px";
             card.appendChild(secondLine);
         let thirdLine = document.createElement( 'h3');
-            thirdLine.textContent = `${this.pages}`;
+            thirdLine.innerHTML = `${this.pages}`;
             thirdLine.style.fontFamily = "monospace";
             thirdLine.style.borderBottom = "dotted 1px black";
             thirdLine.style.paddingTop = "20px";
             card.appendChild(thirdLine);
-        let fourthLine = document.createElement( 'h3');
-            let checkboxed = document.createElement('input');
-            checkboxed.type = "checkbox";
-            checkboxed.value ="value";
-            checkboxed.id = "id";
-            fourthLine.textContent = `${this.hasRead} ${checkboxed}`;
-            fourthLine.style.fontFamily = "monospace";
-            fourthLine.style.borderBottom = "dotted 1px black";
-            fourthLine.style.paddingTop = "20px";
-            card.appendChild(fourthLine);
+        let readBttn = document.createElement( 'button');
+        
+                if(document.getElementById('no').checked ) {
+                    readBttn.textContent = 'Not Read';
+                    readBttn.style.backgroundColor = 'red'
+                } else if (document.getElementById('yes').checked) {
+                    readBttn.textContent = 'Read';
+                    readBttn.style.backgroundColor = 'green';
+                };
+            readBttn.style.fontFamily = "monospace";
+            readBttn.style.color ="white";
+            readBttn.style.height ="22px";
 
-    });
+            readBttn.addEventListener( 'click',()=> {
+                if( readBttn.textContent == 'Not Read') {
+                    readBttn.textContent = 'Read';
+                    readBttn.style.backgroundColor = 'green';
+                } else if (readBttn.textContent == 'Read') {
+                    readBttn.textContent = 'Not Read',
+                readBttn.style.backgroundColor = 'red'
+                }
+            })
+            card.appendChild(readBttn);
+
+
+       
+
+        let deleteButton = document.createElement('button');
+            deleteButton.textContent = "delete";
+            deleteButton.style.height ="22px";
+            deleteButton.style.backgroundColor = "blue"; 
+            deleteButton.style.fontFamily = "monospace";
+            deleteButton.style.borderBottom = "dotted 1px black";
+            deleteButton.style.color = "white";
+           
+            card.appendChild(deleteButton);
+            deleteButton.addEventListener( 'click', () => {
+                library.removeChild(card);
+                myLibrary.splice(Math.floor(Math.random() * myLibrary.length) ,1) [0];;
+            })
+            card.appendChild(deleteButton);
+            
+
+
+            library.appendChild(card);
+   
+           
+            });
+
+        };
+
+
+
+        let rButton = document.querySelectorAll('#rButton');
+        let dButton = document.querySelectorAll('#dButton');
+        for (let i = 0; i < rButton.length; i++) {
+             rButton[i].addEventListener('click',()=>{
+            if (rButton[i].style.backgroundColor == 'green',
+            rButton[i].textContent == "Read") {
+                rButton[i].textContent = 'Not Read';
+                rButton[i].style.backgroundColor = 'red';
+            } else if (rButton[i].style.backgroundColor == 'red') {
+                rButton[i].textContent = 'Read';
+                rButton[i].style.backgroundColor = 'green';
+            }
+         });
+        };
     
-}
+        for (let i = 0; i < dButton.length; i++) {
+        dButton[i].addEventListener('click', ()=> {
+            library.removeChild(books[i]);
+            myLibrary.splice(Math.floor(Math.random() * myLibrary.length) ,1) [0];
+        });
+    };
+console.log(addBookToLibrary());
+console.log(myLibrary);
 
